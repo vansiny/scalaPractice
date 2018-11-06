0055e9253a13a9354acdfb0d224915d29e54096b
hg <hghuguang@126.com>
2018-11-06 22:43:39 +0800

    Test companion class and object private member

--- src/main/scala/com/example/singletonObject/DemoPhone.scala
+++ src/main/scala/com/example/singletonObject/DemoPhone.scala
@@ -1,14 +1,21 @@
 package com.example.singletonObject
 
 class DemoPhone (
-  val user: String,
-  val num: Int){
+  private val user: String,
+  private val num: Int){
   println("traditional:"+user+"'s number is "+num)
   println(s"new way:$user's number is $num")
+
+  //companion class can visit private member of companion object
+  def getName = DemoPhone.user
 }
 
 object DemoPhone extends App {
+  private val user:String = "Mike"
   var phone1: DemoPhone=new DemoPhone ("Jim",12345)
+  //companion object can access private member of companion class
+  println(phone1.num)
+  println(phone1.getName)
 
   object CellPhone extends DemoPhone("Tom",1234)
 
--- src/main/scala/com/example/singletonObject/TestObject.scala
+++ src/main/scala/com/example/singletonObject/TestObject.scala
@@ -1,5 +1,7 @@
 package com.example.singletonObject
 
-object TestObject {
+object TestObject extends App {
+  var phone1: DemoPhone=new DemoPhone ("Jim",12345)
+  println(phone1)
 
 }
